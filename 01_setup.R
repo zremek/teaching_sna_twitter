93f32f6f2ac062f3674f0ec1c06373a1f8126c14
Remigiusz Żulicki <remigiuszzulicki@gmail.com>
2022-10-27 14:02:17 +0200

    start setup

---
+++ 01_setup.R
@@ -0,0 +1,13 @@
+# setup
+
+library(rtweet)
+library(tidyverse)
+library(httpuv)
+library(tidytext)
+
+# source: https://resulumit.com/teaching/twtr_workshop.html#1 
+# https://github.com/resulumit/twtr_workshop/tree/materials 
+
+# academic research access
+# https://developer.twitter.com/en/products/twitter-api/academic-research 
+
